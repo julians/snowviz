3db1c6960f5e2d346b408bbc00f6d8e1b40666c4
Julian Stahnke <mail@julianstahnke.com>
2013-01-28 22:05:47 +0100

    select date via timeline, move map and timeline into one script

--- web/timeline.js
+++ web/timeline.js
@@ -1,5 +1,81 @@
 $(document).ready(function ()
 {
+	// map
+	var add1 = 1572/2*0.0050865689142654;
+	var add2 = 151*0.0050865689142654;
+	var bounds = new L.LatLngBounds([38.7, 67], [38.7+add2, 67+add1]);
+	
+	var map = L.map('map', {
+        center: bounds.getCenter(),
+        zoom : 10,
+		attributionControl: false
+    });
+
+	L.tileLayer('http://localhost:8888/v2/Snowviz_cd4e28/{z}/{x}/{y}.png').addTo(map);
+	
+	var currentLayer = new L.layerGroup().addTo(map);
+	var firstDate = moment("2012-01-01");
+	var lastDate = moment("2012-01-01").add("days", 151);
+	var currentDate = moment("2012-01-01");
+	
+    $(window).keypress(function (event) {
+		console.log("keypress");
+        if (event.charCode == 107) {
+            previousDate();
+        } else if (event.charCode == 106) {
+            nextDate();
+        }
+    });
+	
+	var currentCoverageLayer = null;
+	
+	getDataForCurrentDate();
+	
+	function getDataForCurrentDate()
+	{
+		console.log("getting data");
+		$.getJSON('http://localhost:5000/date/'+currentDate.format("YYYY-MM-DD")+'?callback=?', function(data) {
+			var cellsize = data["cellsize"];
+			console.log("got some data");
+			
+			var newCoverageLayer = new L.TileLayer.MaskCanvas({
+				'opacity': 0.5,
+				radius: 750,
+				useAbsoluteRadius: true
+			});
+			
+			newCoverageLayer.options.rectSize = data["cellsize"];
+			newCoverageLayer.setData(data["points"]);
+			if (currentCoverageLayer != null) map.removeLayer(currentCoverageLayer);
+			currentCoverageLayer = newCoverageLayer;
+			map.addLayer(newCoverageLayer);
+			console.log("yay");
+		});
+	}
+	function goToDate(dateString)
+	{
+		var newDate = moment(dateString);
+		if (newDate != currentDate && newDate >= firstDate && newDate <= lastDate) {
+			currentDate = newDate;
+			getDataForCurrentDate();
+		}
+	}
+	function previousDate()
+	{
+		console.log("f1");
+		if (currentDate > firstDate) currentDate.subtract("days", 1);
+		console.log("previous date: " + currentDate.format("YYYY-MM-DD"));
+		getDataForCurrentDate();
+	}
+	function nextDate()
+	{
+		console.log("f1");
+		if (currentDate < lastDate) currentDate.add("days", 1);
+		console.log("next date: " + currentDate.format("YYYY-MM-DD"));
+		getDataForCurrentDate();
+	}
+	
+	// timeline
 	paper.install(window);
 	
 	// set up graph
@@ -46,8 +122,13 @@ $(document).ready(function ()
 			hover_text.visible = false;
 		}
 	}
-	hover_pointer.onMouseMove = function (event) {
+	hover_pointer.onMouseUp = function (event) {
 		paper.projects[1].activate();
+		// if event is inside canvas AND we have data
+		if (event.point.x >= 0 && event.point.x <= view.viewSize.width && event.point.y >= 0 && event.point.y <= view.viewSize.height && data) {
+			var dayData = getGraphDataForPosition(event.point.x);
+			goToDate(dayData["day"]["date"])
+		}
 	}
 	
 	function getGraphDataForPosition (x)

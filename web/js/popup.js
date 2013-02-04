6ecca44bbb6ff90657d7396903f323b6337636ec
Florian Schulz <mail@getflourish.com>
2013-02-04 22:50:57 +0100

    Download Popup

---
+++ web/js/popup.js
@@ -0,0 +1,17 @@
+(function( $ ){
+
+  $.fn.popup = function( options ) {  
+
+    // Create some defaults, extending them with any options that were provided
+    var settings = $.extend( {
+      'content'         : '',
+    }, options);
+
+    return this.each(function() {        
+
+		
+
+    });
+
+  };
+})( jQuery );
